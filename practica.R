5d7d66aa7ee850efae7024a6b5ef7bf44db8a017
unknown <7209270822@untrm.edu.pe>
2025-09-02 15:57:25 -0500

    clase pratica

---
+++ practica.R
@@ -0,0 +1,37 @@
+# mi primera practica con R
+
+# alt + -
+
+x <- "hola"
+y <- "untrm"
+
+# F(x, y) = x + 2
+
+paste (x, y)
+
+paste (x, y, sep = " +")
+
+paste (X, y)
+
+# tidyverse cran
+
+library(tidyverse)
+
+# pipe
+
+a1 <- "estudiante"
+1a <- "cocina"
+
+a_1 <- "objeto"
+a.1 <- "a"
+
+
+notas <- c(16, 17, 19, 15)
+media <- mean(notas)
+media
+
+# cntrl + shift +m -> %>%
+
+
+m <- c(16, 17, 19, 15) %>%
+  mean()
